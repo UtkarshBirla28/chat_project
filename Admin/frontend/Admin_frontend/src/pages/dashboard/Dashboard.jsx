f1b0d8a7295c697cdb6822872b1de836d5ab3a46
utkarsh birla <utkarshbirla3@gmail.com>
2024-12-11 14:34:52 +0530

    [UB] - heading added

--- Admin/frontend/Admin_frontend/src/pages/dashboard/Dashboard.jsx
+++ Admin/frontend/Admin_frontend/src/pages/dashboard/Dashboard.jsx
@@ -4,5 +4,6 @@ import { dashboardColumn } from "../../utils/dashboardColumn";
 
 export const Dashboard = () => {
   const { data, error, loading } = useDashboard();
+  <h1 className="text-center text-3xl ">Admin Portal </h1>;
   return <div>{<DataTable tableData={data} column={dashboardColumn} />}</div>;
 };
